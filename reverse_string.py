4b5aff12cd9ef0586aab2dda3eb126ead929223e
utishtr-jpg <utishtr@gmail.com>
2026-02-22 20:40:46 -0800

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,10 @@
+string=input("Please enter your own string:")
+
+string2=("")
+
+for i in string:
+    string2=i+string2
+
+print("The original string you entered is",string)
+print("The reversed version of your string is", string2)
+#end
